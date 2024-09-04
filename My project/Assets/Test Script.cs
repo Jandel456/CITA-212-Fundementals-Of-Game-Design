e1cb44e534d7a34444b96cfb41cd206dcdc777e2
Jandel <127899373+Jandel456@users.noreply.github.com>
2024-09-04 13:45:53 -0400

    Made a script that adds movement to a character
    The script uses if statements to move around.

---
+++ My project/Assets/Test Script.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float moveSpeed = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Booting Up, Beep Boop");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            if (Input.GetKey(KeyCode.D))
+    {
+        transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        
+    }
+    else if (Input.GetKey(KeyCode.A))
+    {
+        transform.position += Vector3.right * -moveSpeed * Time.deltaTime;
+        
+    }
+
+    else if (Input.GetKey(KeyCode.W))
+    {
+        transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+
+    }
+    else if (Input.GetKey(KeyCode.S))
+    {
+        transform.position += Vector3.up * -moveSpeed * Time.deltaTime;
+
+    }
+    }
+}
