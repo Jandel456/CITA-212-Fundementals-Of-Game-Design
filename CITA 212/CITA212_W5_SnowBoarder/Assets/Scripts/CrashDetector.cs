9c711b25be3768a1aedc83adad2cbe7ab04b0773
Jandel <jandel456@gmail.com>
2024-09-30 12:46:41 -0400

    Implemented FindObjectOfType to interact with game objects

--- CITA 212/CITA212_W5_SnowBoarder/Assets/Scripts/CrashDetector.cs
+++ CITA 212/CITA212_W5_SnowBoarder/Assets/Scripts/CrashDetector.cs
@@ -6,11 +6,15 @@ using UnityEngine.SceneManagement;
 public class CrashDetector : MonoBehaviour
 {
     [SerializeField] float LoadDelay = 0.5f;
+    [SerializeField] ParticleSystem CrashEffect;
+
 
     void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.tag == "Ground")
         {
+            CrashEffect.Play();
+
             Debug.Log("You Crashed!");
 
             Invoke("ReloadScene", LoadDelay);
--- CITA 212/CITA212_W5_SnowBoarder/Assets/Scripts/FinishLine.cs
+++ CITA 212/CITA212_W5_SnowBoarder/Assets/Scripts/FinishLine.cs
@@ -6,11 +6,14 @@ using UnityEngine.SceneManagement;
 public class FinishLine : MonoBehaviour
 {
     [SerializeField] float LoadDelay = 1f;
+    [SerializeField] ParticleSystem FinishEffect;
 
     void OnTriggerEnter2D(Collider2D other) 
     {
         if (other.tag == "Player")
         {
+            FinishEffect.Play();
+
             Debug.Log("You Finished!");
 
             Invoke("ReloadScene", LoadDelay);
