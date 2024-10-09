85419fae16e76d6325f34bbec0fcc486aa605362
Jandel <jandel456@gmail.com>
2024-10-09 12:38:34 -0400

    Created ScriptableObject for storing quiz questions and answers.
    I also deleted the other folder for CITA 255

---
+++ CITA 212/QuizMaster/Assets/Scripts/QuestionSO.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Quiz Question", fileName = "New Question")]
+public class QuestionSO : ScriptableObject
+{
+    [TextArea(2,6)]     //allows us to change how many lines of text can fit in the serialized field to make it easier to edit questions in the future, the user wont see it, only the devs will.
+    [SerializeField] string Question = "Enter new question text here";
+}
