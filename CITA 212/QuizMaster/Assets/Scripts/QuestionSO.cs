5dc7421e9bc63164015a1f457a16f204e711fd35
Jandel <jandel456@gmail.com>
2024-10-09 12:42:17 -0400

    Implemented getter methods for accessing quiz data.

--- CITA 212/QuizMaster/Assets/Scripts/QuestionSO.cs
+++ CITA 212/QuizMaster/Assets/Scripts/QuestionSO.cs
@@ -6,5 +6,10 @@ using UnityEngine;
 public class QuestionSO : ScriptableObject
 {
     [TextArea(2,6)]     //allows us to change how many lines of text can fit in the serialized field to make it easier to edit questions in the future, the user wont see it, only the devs will.
-    [SerializeField] string Question = "Enter new question text here";
+    [SerializeField] string question = "Enter new question text here";
+
+    public string GetQuestion()
+    {
+        return question;
+    }
 }
