73d28c8747668b81f8c49836b34c7cd1537fa9d0
Jandel <jandel456@gmail.com>
2024-10-09 13:20:18 -0400

    Used for loop to populate answer buttons with options.

--- CITA 212/QuizMaster/Assets/Scripts/QuizScript.cs
+++ CITA 212/QuizMaster/Assets/Scripts/QuizScript.cs
@@ -7,8 +7,18 @@ public class QuizScript : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
+    [SerializeField] GameObject[] answerbuttons;
+
     void Start()
     {
         questionText.text = question.GetQuestion();
+
+        for(int i = 0; i < answerbuttons.Length; i++)
+        // this will look through the cildren of answer buttons and find the first textmeshpro component that it can
+        // then it will change the text of that button to answer of our stored question
+        {
+        TextMeshProUGUI buttonText = answerbuttons[i].GetComponentInChildren<TextMeshProUGUI>();        
+        buttonText.text = question.GetAnswer(i);
+        }
     }
 }
