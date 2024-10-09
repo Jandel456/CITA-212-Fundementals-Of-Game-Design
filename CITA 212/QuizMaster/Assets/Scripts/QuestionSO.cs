6ec34c3ac3c3b429d52bc58376e59abc2a9c3eea
Jandel <jandel456@gmail.com>
2024-10-09 12:59:11 -0400

    Used an array to store answer options and get the correct answer  index.

--- CITA 212/QuizMaster/Assets/Scripts/QuestionSO.cs
+++ CITA 212/QuizMaster/Assets/Scripts/QuestionSO.cs
@@ -7,9 +7,20 @@ public class QuestionSO : ScriptableObject
 {
     [TextArea(2,6)]     //allows us to change how many lines of text can fit in the serialized field to make it easier to edit questions in the future, the user wont see it, only the devs will.
     [SerializeField] string question = "Enter new question text here";
+    [SerializeField] string[] answers = new string [4];
+    [SerializeField] int correctAnswerIndex; 
 
     public string GetQuestion()
     {
         return question;
     }
+    public string GetAnswer(int index)      // this is returning the answer as a string aat the index that we specified
+    {
+        return answers[index];
+    }
+    public int GetCorretAnswerIndex()
+    {
+        return correctAnswerIndex;
+    }
+
 }
