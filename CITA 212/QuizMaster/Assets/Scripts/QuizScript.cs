0190247301d245372277da16ec6257feac89059a
Jandel <jandel456@gmail.com>
2024-10-26 15:45:33 -0400

    Implemented answer feedback with sprite swapping
    Linked each answer button to the appropriate quiz answer.
    Displayed correct or incorrect messages based on the user’s selection.

--- CITA 212/QuizMaster/Assets/Scripts/QuizScript.cs
+++ CITA 212/QuizMaster/Assets/Scripts/QuizScript.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using Microsoft.Unity.VisualStudio.Editor;
+using UnityEngine.UI;
+using Image = UnityEngine.UI.Image;
 
 public class QuizScript : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI questionText;
     [SerializeField] QuestionSO question;
     [SerializeField] GameObject[] answerbuttons;
+    int correctAnswerIndex;
+    [SerializeField] Sprite defaultAnswerSprite;
+    [SerializeField] Sprite correctAnswerSprite;
 
     void Start()
     {
@@ -19,6 +25,27 @@ public class QuizScript : MonoBehaviour
         {
         TextMeshProUGUI buttonText = answerbuttons[i].GetComponentInChildren<TextMeshProUGUI>();        
         buttonText.text = question.GetAnswer(i);
+        }
+    }
+
+    public void OnAnswerSelected(int index)
+    {
+        Image buttonImage;
+
+        if(index == question.GetCorretAnswerIndex())
+        {
+            questionText.text = "Correct!"; // "Correct!" for now is a placeholder, we might want to add more details like why its correct
+            buttonImage = answerbuttons[index].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
+        }
+        else
+        {
+            correctAnswerIndex = question.GetCorretAnswerIndex();
+            string correctAnswer = question.GetAnswer(correctAnswerIndex);
+            questionText.text = "Sorry the correct answer was;\n" + correctAnswer;
+
+            buttonImage = answerbuttons[correctAnswerIndex].GetComponent<Image>();
+            buttonImage.sprite = correctAnswerSprite;
         }
     }
 }
