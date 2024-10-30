7a54ef128bac0680d1469fda7d3cbd0ec2cdcddb
Jandel <jandel456@gmail.com>
2024-10-30 12:42:46 -0400

    Implemented player movement with Unity Input System.

---
+++ CITA 212/Laser Defender/Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Profiling;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    Vector2 rawInput;
+    [SerializeField] float moveSpeed = 10f;
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
+        transform.position += delta;
+    }
+
+    void OnMove(InputValue value)
+    {
+        rawInput = value.Get<Vector2>();
+        Debug.Log(rawInput);
+    }
+}
