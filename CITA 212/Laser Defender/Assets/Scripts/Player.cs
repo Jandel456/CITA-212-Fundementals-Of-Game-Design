fd693db688ba78282e75a35b7da2c3fe66f1e298
Jandel <jandel456@gmail.com>
2024-10-30 13:01:07 -0400

    Set player boundaries using ViewportToWorldPoint.

--- CITA 212/Laser Defender/Assets/Scripts/Player.cs
+++ CITA 212/Laser Defender/Assets/Scripts/Player.cs
@@ -1,23 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Numerics;
 using UnityEditor.Profiling;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Vector2 = UnityEngine.Vector2;
+using Vector3 = UnityEngine.Vector3;
 
 public class Player : MonoBehaviour
 {
     Vector2 rawInput;
     [SerializeField] float moveSpeed = 10f;
 
+    [SerializeField] float paddingleft;
+    [SerializeField] float paddingright;
+    [SerializeField] float paddingtop;
+    [SerializeField] float paddingbottom;
+
+    Vector2 minBounds;
+    Vector2 maxBounds;
+
+    void Start()
+    {
+        InitBounds();
+    }
+
     void Update()
     {
         Move();
     }
 
+    void InitBounds()
+    {
+        Camera mainCamera = Camera.main;
+        minBounds = mainCamera.ViewportToWorldPoint(new Vector2(0,0));
+        maxBounds = mainCamera.ViewportToWorldPoint(new Vector2(1,1));
+    }
+
     void Move()
     {
-        Vector3 delta = rawInput * moveSpeed * Time.deltaTime;
-        transform.position += delta;
+        Vector2 delta = rawInput * moveSpeed * Time.deltaTime;
+        Vector2 newPos = new Vector2(); // allows us to account for boundries of player movement.
+        newPos.x = Mathf.Clamp(transform.position.x + delta.x, minBounds.x + paddingleft, maxBounds.x - paddingright);       // this is what restricts us from moving beyond the screen horizontally
+        newPos.y = Mathf.Clamp(transform.position.y + delta.y, minBounds.y + paddingbottom, maxBounds.y - paddingtop);       // this is what restricts us from moving beyond the screen vertically
+        transform.position = newPos;
     }
 
     void OnMove(InputValue value)
