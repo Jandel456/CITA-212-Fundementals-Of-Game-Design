53ea810c81c8ac8195a7e598a0ba18467b01e308
Jandel <jandel456@gmail.com>
2024-10-30 13:44:53 -0400

    Implemented enemy pathfinding along predefined paths.

---
+++ CITA 212/Laser Defender/Assets/Scripts/Pathfinder.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pathfinder : MonoBehaviour
+{
+[SerializeField] WaveConfigSO waveConfig;
+List<Transform> waypoints;
+int waypointIndex = 0;
+
+    void Start()
+    {
+        waypoints = waveConfig.GetWaypoints();
+        transform.position = waypoints[waypointIndex].position;
+    }
+
+    void Update()
+    {
+        FollowPath();
+    }
+
+    void FollowPath()
+    {
+        if(waypointIndex < waypoints.Count)
+        {
+            Vector3 targetPostion = waypoints[waypointIndex].position;                              // this shows us where we are and where we are going
+            float delta = waveConfig.GetMoveSpeed() * Time.deltaTime;                               // this shows us how fast we are going, so this is the distance we move each frame.
+            transform.position = Vector2.MoveTowards(transform.position, targetPostion, delta);     // this is what actually what moves our enemy to our next waypoint.
+            if (transform.position == targetPostion)
+            {
+                waypointIndex++; // gives us the next waypoint we have to follow.
+            }
+        }
+        else
+        {
+            Destroy(gameObject);    // once we're done following the waypoints, we killourselves. Yippe!!!!
+        }
+    }
+}
