c5683ae550a506033aba7c8f55f6805182f143d9
Jandel <jandel456@gmail.com>
2024-11-04 12:51:51 -0500

    Implemented enemy spawning with Coroutines.

---
+++ CITA 212/Laser Defender/Assets/Scripts/EnemySpawner.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    [SerializeField] List<WaveConfigSO> WaveConfigs;
+    [SerializeField] float timeBeweenWaves = 0f;
+    [SerializeField] WaveConfigSO currentWave;
+
+    void Start()
+    {
+        StartCoroutine(SpawnEnemyWaves());
+    }
+
+    public WaveConfigSO GetCurrentWave()
+    {
+        return currentWave;
+    }
+
+    IEnumerator SpawnEnemyWaves()
+    {
+        foreach (WaveConfigSO wave in WaveConfigs)
+        {
+            currentWave = wave;
+            for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+            {
+                // this will create the enemy prefab runtime and transform position of prefab.
+                Instantiate(currentWave.GetEnemyPrefab(i), currentWave.GetStartingWaypoint().position, Quaternion.identity, transform);
+                yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+            }
+            yield return new WaitForSeconds(timeBeweenWaves);
+        }
+
+    }
+
+}
--- CITA 212/Laser Defender/Assets/Scripts/WaveConfigSO.cs
+++ CITA 212/Laser Defender/Assets/Scripts/WaveConfigSO.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,8 +6,22 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "Wave Config", fileName = "New Wave Config")]
 public class WaveConfigSO : ScriptableObject
 {
+    [SerializeField] List<GameObject> enemyPrefabs;
     [SerializeField] Transform pathPrefab;
     [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float timeBetweenEnemySpawns = 1f;
+    [SerializeField] float spawnTimeVariance = 0f;      // will eb used to add and subtract from the time between enemy spawns, set to 0 for no variance
+    [SerializeField] float minimumSpawnTime = 0.2f;       // incase spawn time varaince makes a negative spawn time, this will make sure the enemies have spawn time that can be used.
+
+    public int GetEnemyCount()
+    {
+        return enemyPrefabs.Count;
+    }
+
+    public GameObject GetEnemyPrefab(int index)
+    {
+        return enemyPrefabs[index];
+    }
 
     public Transform GetStartingWaypoint()
     {
@@ -26,6 +41,12 @@ public class WaveConfigSO : ScriptableObject
     public float GetMoveSpeed()
     {
         return moveSpeed;
+    }
+
+    public float GetRandomSpawnTime()
+    {
+        float spawnTime = UnityEngine.Random.Range(timeBetweenEnemySpawns - spawnTimeVariance, timeBetweenEnemySpawns + spawnTimeVariance);
+        return Mathf.Clamp(spawnTime, minimumSpawnTime, float.MaxValue);
     }
 
 }
